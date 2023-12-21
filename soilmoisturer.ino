9d8f035d9d929c68e1c2085f55ddc674270f12e7
Amith Abey Stephen <145148320+Amith-Abey-Stephen@users.noreply.github.com>
2023-12-21 10:56:30 +0530

    Update soilmoisturer.ino

--- soilmoisturer.ino
+++ soilmoisturer.ino
@@ -1,5 +1,7 @@
-int green=2;
-int sensorPin=A0;
+//initialising the pins
+
+int green=2;// pin for a led
+int sensorPin=A0;//pin on the soil moisture sensor
 
 void setup() {  
   Serial.begin(9600);
@@ -8,10 +10,10 @@ void setup() {
 
 void loop() {
  
-  int moisture = analogRead(sensorPin);
-  Serial.print("Analog output: ");
-  Serial.println(moisture);
-  if (moisture < 400) {
+  int moisture = analogRead(sensorPin);//reading the values
+  Serial.print("Analog output: ");//printing in monitor
+  Serial.println(moisture);//printing in monitor
+  if (moisture < 400) {//condition
     Serial.println("Status: Soil is wet");
     digitalWrite(green,HIGH);
     delay(1000);
